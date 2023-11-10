3fb6ccedd44c827f844a0db7f844fbceb1becfd1
MichaelMantovani <michmantovani@gmail.com>
2023-11-10 14:35:48 +0100

    finish snack1

--- src/org/lessons/java/snack1.java
+++ src/org/lessons/java/snack1.java
@@ -1,5 +1,16 @@
 package org.lessons.java;
 
-public class snack1 {
+import java.util.Scanner;
 
+public class snack1 {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("Inserisci un numero?");
+	int number = in.nextInt();
+	if (number % 2 == 0) {
+		System.out.println(number);
+	}else {
+		System.out.println(++number);
+	}
+}
 }
