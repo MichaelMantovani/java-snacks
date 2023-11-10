3d4841af8fab58123dba2ff3dd0e81ddc1adb508
MichaelMantovani <michmantovani@gmail.com>
2023-11-10 16:12:32 +0100

    add and finish snack5

--- src/org/lessons/java/snack4.java
+++ src/org/lessons/java/snack4.java
@@ -6,19 +6,24 @@ public class snack4 {
 public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
 	
+//	Chiedo una parola all'utente
 	System.out.println("Dammi una parola?");
 	String word = in.nextLine();
 	
+	in.close();
+	
+//	Creo un array con le lettere della parola scelta
 	char[] wordArray = word.toCharArray();
 	
+//	Genero la parola al contrario
 	String reverseWord = "";
-	
 	for (int x=wordArray.length -1; x >= 0 ; x--) {
 		
 		reverseWord += wordArray[x];
 	}
 	
 	
+//	Controllo se la parola è palindroma
 	if(word.equalsIgnoreCase(reverseWord)) {
 		System.out.println("La parola scelta è palindroma");
 	} else {
---
+++ src/org/lessons/java/snack5.java
@@ -0,0 +1,45 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class snack5 {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+		
+	boolean isZero = false;
+		while (isZero == false) {
+			System.out.println("Scrivi un codice alfanumerico (può contenere anche simboli)");
+			
+			String input = in.nextLine();
+			char[] inputArray = input.toCharArray();
+			
+			int lettersCount = 0;
+			int simbolsCount = 0;
+			int numbersCount = 0;
+			for (int x=0; x< inputArray.length; x++) {
+				if(inputArray[x] == '0') {
+					numbersCount++;
+					isZero = true; 
+					break;
+				}
+				else if(Character.isDigit(inputArray[x])) {
+					numbersCount++;
+				}else if (Character.isLetter(inputArray[x])){
+					lettersCount++;
+				} else{
+					simbolsCount++;
+				}
+				
+			}
+			System.out.println("Le lettere sono: " + lettersCount);
+			System.out.println("I numeri sono: " + numbersCount);
+			System.out.println("I simboli sono: " + simbolsCount);
+		}
+		
+		
+	
+	
+
+
+}
+}
